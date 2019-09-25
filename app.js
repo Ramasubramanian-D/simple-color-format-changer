7a26b2e43460d4d5eec1f5f8ca4ca4a75c1004c0
ramasubramanian <d.ramasubramanian.1997@gmail.com>
2019-09-26 00:09:25 +0530

    add rgb to hsl formatting

--- app.js
+++ app.js
@@ -26,6 +26,48 @@ module.exports.rgbToHex = (red, green, blue) => {
     return hex;
 }
 
+module.exports.rgbToHsl = (red, green, blue) => {
+    red = parseInt(red)/255;
+    green = parseInt(green)/255;
+    blue = parseInt(blue)/255;
+
+    let channelMin = Math.min(red,green,blue);
+    let channelMax = Math.max(red,green,blue);
+    let delta = channelMax - channelMin;
+
+    let hue,saturation,light;
+    if (delta == 0)
+        hue = 0;
+    else if(channelMax == red)
+        hue = (((green - blue) / delta) % 6);
+    else if(channelMax == green)
+        hue = (((blue - red ) / delta) + 2);    
+    else 
+        hue = (((red - green) / delta) + 4);
+    
+    hue = Math.round(hue);
+    
+    if (hue < 0)
+        hue += 360;
+
+    light = ((channelMax + channelMin) / 2);
+
+    if  (delta == 0)
+        saturation = 0;
+    else 
+        saturation = delta / (1 - Math.abs(2 * light - 1))
+
+    saturation = +(saturation * 100).toFixed(1);
+    light = +(light * 100).toFixed(1);
+
+    let hsl = new Object;
+    hsl.hue = hue;
+    hsl.saturation = saturation;
+    hsl.light = light;
+    hsl.combined = hue+","+saturation+","+light;
+    
+}
+
 const toHex = (decimalVal) => {
     if(decimalVal > 255) {
         decimalVal = 255;
